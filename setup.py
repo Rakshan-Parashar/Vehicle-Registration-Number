a5d2ea59adee5f8692ef3e08e258571fbf3b9ff8
Rakshan-Parashar <rakshanparashar265@gmail.com>
2026-05-24 17:26:41 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Rakshan Parashar',
+    author_email='rakshanparashar265@gmail.com',
+    packages=find_packages()
+)
